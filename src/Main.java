53b8ba571f3cd57021c9cbe04102fe70dd4f2e5b
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-16 18:44:13 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Läs in en text och en längd, skicka båda till en
+        // metod som ser till att texten blir så lång.
+        //   - Är texten längre = ta de sista X tecknen
+        //   - Är texten för kort = lägg till space före
+
+
+    }
+}
